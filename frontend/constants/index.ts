0c31e46790205f12e09986d6af388d93b7e22d09
fuzail-oddtech <fuzail@oddtech.com>
2026-01-15 00:41:50 +0500

    frontend attached to cloud backend

--- frontend/constants/index.ts
+++ frontend/constants/index.ts
@@ -1,7 +1,9 @@
 // export const BASE_URL = 'http://192.168.100.85:8001/'; //backend local Host
 // export const BASE_URL = 'http://localhost:8001/'; //backend local Host
-export const BASE_URL = 'http://192.168.0.116:8001/';
-export const EXPO_PUBLIC_API_BASE = 'https://aeronautically-uncarpentered-dorthey.ngrok-free.dev';
+export const BASE_URL = 'http://192.168.110.117:8001/';
+// export const EXPO_PUBLIC_API_BASE = 'https://aeronautically-uncarpentered-dorthey.ngrok-free.dev';
+export const EXPO_PUBLIC_API_BASE = 'http://192.168.110.117:8001/';
+// export const EXPO_PUBLIC_API_BASE = 'https://society-management-system-lerv.onrender.com';
 
 // const client = StreamChat.getInstance('6rjwnb9f7j5a', 'n7hs49nr55bpbsvatmsnf9uxece4uq7npspk849kuqnwfmyy4nbgcn4rnvpbumjg');
 // const client = StreamChat.getInstance('sthbqw6sepp8', 'd4ayewedkqjxy4s4k9qshb495c8hsm7e6z9jqpas47wc4dk5efx92atn3v94dbgg');
